fbea3e134bd0e39eb392125430f4bf17bfe4f188
Tatumc4561 <tatumc4561@gmail.com>
2022-06-10 11:30:30 -0700

    js for anon user

--- static/JS/nologinApp.js
+++ static/JS/nologinApp.js
@@ -4,3 +4,15 @@ const profileOnly = document.querySelectorAll('.profile-only')
 for(el of profileOnly){
     el.style = "visibility: hidden;"
 }
+
+
+// top-nav
+
+const searchDropdownButton = document.querySelector('#search-button')
+const searchDropdownMenu = document.querySelector('#search-dropdownz')
+
+searchDropdownButton.addEventListener('click', function(){
+
+    searchDropdownMenu.classList.toggle('search-dropdown-wrap')
+    console.log('searching bro')
+})
