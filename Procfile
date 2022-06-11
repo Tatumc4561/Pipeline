53740bd540bc5bc21f42d15e3d11a34cbfe717a2
Tatumc4561 <tatumc4561@gmail.com>
2022-06-10 23:53:11 -0700

    update

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn pipeline_proj.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
 manage.py migrate
--- pipeline_proj/settings/production.py
+++ pipeline_proj/settings/production.py
@@ -1,7 +1,12 @@
 from pipeline_proj.settings.common import *
+import dotenv
 
 DEBUG = False
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["the-pipeline.herokuapp.com"]
