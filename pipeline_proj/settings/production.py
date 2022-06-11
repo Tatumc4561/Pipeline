16e462e9fba0533f362bd916cb01d46d9125eab5
Tatumc4561 <tatumc4561@gmail.com>
2022-06-11 00:00:15 -0700

    update

--- pipeline_proj/settings/production.py
+++ pipeline_proj/settings/production.py
@@ -1,12 +1,8 @@
 from pipeline_proj.settings.common import *
-import dotenv
 
 DEBUG = False
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ["the-pipeline.herokuapp.com"]
