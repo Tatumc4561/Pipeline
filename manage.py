cc2b5fe5fc106f11637f1114d2692f8d8188badd
Tatumc4561 <tatumc4561@gmail.com>
2022-06-11 11:56:06 -0700

    dev

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pipeline_proj.settings.production")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "pipeline_proj.settings.development"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
