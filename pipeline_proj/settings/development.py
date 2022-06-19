1bd53938ea99c5cb37a2ca5ee8be58a9f19853e8
Tatumc4561 <tatumc4561@gmail.com>
2022-06-19 00:08:01 -0700

    d

--- pipeline_proj/settings/development.py
+++ pipeline_proj/settings/development.py
@@ -22,7 +22,7 @@ from pipeline_proj.settings.common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["the-pipeline.herokuapp.com"]
 
