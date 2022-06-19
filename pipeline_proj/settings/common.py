9647a77627b3efd8186a82cdce358dc484459b89
Tatumc4561 <tatumc4561@gmail.com>
2022-06-18 23:41:54 -0700

    dropbox

--- pipeline_proj/settings/common.py
+++ pipeline_proj/settings/common.py
@@ -138,12 +138,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# dropbox
-DEFAULT_FILE_STORAGE = "storages.backends.dropbox.DropBoxStorage"
-
-DBX_TOKEN = os.environ.get("DBX_TOKEN")
-DROPBOX_OAUTH2_TOKEN = DBX_TOKEN
-DROPBOX_ROOT_PATH = '/media/'
 
 # import django_on_heroku
 
--- pipeline_proj/settings/production.py
+++ pipeline_proj/settings/production.py
@@ -6,3 +6,14 @@ DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ["the-pipeline.herokuapp.com"]
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# dropbox
+DEFAULT_FILE_STORAGE = "storages.backends.dropbox.DropBoxStorage"
+
+DBX_TOKEN = os.environ.get("DBX_TOKEN")
+DROPBOX_OAUTH2_TOKEN = DBX_TOKEN
+DROPBOX_ROOT_PATH = "/media/"
