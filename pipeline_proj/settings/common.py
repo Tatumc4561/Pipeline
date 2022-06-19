2050713cb59b1f7c48d7901e7fdbadf4dabc38cc
Tatumc4561 <tatumc4561@gmail.com>
2022-06-19 00:05:48 -0700

    d

--- pipeline_proj/settings/common.py
+++ pipeline_proj/settings/common.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "threads",
     # "el_pagination",
     "treebeard",  # API Library implements efficient tree navigation
-    "whitenoise.runserver_nostatic",
+    # "whitenoise.runserver_nostatic",
 ]
 
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "pipeline_proj.urls"
