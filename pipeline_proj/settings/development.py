9b085aed38a0abb900c6e5bfa38e7d6d9d492d51
Tatumc4561 <tatumc4561@gmail.com>
2022-06-19 23:55:38 -0700

    edit

--- pipeline_proj/settings/development.py
+++ pipeline_proj/settings/development.py
@@ -1,40 +1,15 @@
-# from pipeline_proj.settings.common import *
-# import dotenv
-
-# DEBUG = True
-
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
-#     dotenv.load_dotenv(dotenv_file)
-
-# SECRET_KEY = os.environ["SECRET_KEY"]
-
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-
-# ALLOWED_HOSTS = ["127.0.0.1"]
-
-
-# ---------------------------  Production.py not running?
-
 from pipeline_proj.settings.common import *
+import dotenv
 
+DEBUG = True
 
-DEBUG = False
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["the-pipeline.herokuapp.com"]
 
+# SECURITY WARNING: don't run with debug turned on in production!
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
-# dropbox
-DEFAULT_FILE_STORAGE = "storages.backends.dropbox.DropBoxStorage"
-
-
-DBX_TOKEN = os.environ["DBX_TOKEN"]
-DROPBOX_OAUTH2_TOKEN = DBX_TOKEN
-
-DROPBOX_ROOT_PATH = "/media/"
+ALLOWED_HOSTS = ["127.0.0.1"]
